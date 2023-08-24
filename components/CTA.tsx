bad930908f40be9767ac46a1f11014fd88b5978c
holabayor <liasu.olabayo@gmail.com>
2023-08-24 23:18:32 +0100

    testing

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -3,20 +3,55 @@ import React from 'react';
 
 export default function CTA() {
   return (
-    <div className="flex flex-col items-center rounded-md max-w-[250px]">
-      <h4 className="uppercase font-semibold">expert advisory</h4>
-      <Image
-        src="/hands.png"
-        alt="cta"
-        width={40}
-        height={40}
-        className="object-contain"
-      />
+    <div className="flex flex-row flex-wrap py-6">
+      <div className="flex flex-col items-center rounded-md max-w-[250px] bg-red-400">
+        <h4 className="uppercase font-semibold">expert advisory</h4>
+        <Image
+          src="/hands.png"
+          alt="cta"
+          width={40}
+          height={40}
+          className="object-contain"
+        />
 
-      <p className="p-2 text-xs text-center">
-        Get expert advisory from the moment you purchase your car. Consultation
-        on vehicle choice, insurance options, financing and tracking.
-      </p>
+        <p className="p-2 text-xs text-center">
+          Get expert advisory from the moment you purchase your car.
+          Consultation on vehicle choice, insurance options, financing and
+          tracking.
+        </p>
+      </div>
+      <div className="flex flex-col items-center rounded-md max-w-[250px] bg-red-400">
+        <h4 className="uppercase font-semibold">expert advisory</h4>
+        <Image
+          src="/hands.png"
+          alt="cta"
+          width={40}
+          height={40}
+          className="object-contain"
+        />
+
+        <p className="p-2 text-xs text-center">
+          Get expert advisory from the moment you purchase your car.
+          Consultation on vehicle choice, insurance options, financing and
+          tracking.
+        </p>
+      </div>
+      <div className="flex flex-col items-center rounded-md max-w-[250px] bg-red-400">
+        <h4 className="uppercase font-semibold">expert advisory</h4>
+        <Image
+          src="/hands.png"
+          alt="cta"
+          width={40}
+          height={40}
+          className="object-contain"
+        />
+
+        <p className="p-2 text-xs text-center">
+          Get expert advisory from the moment you purchase your car.
+          Consultation on vehicle choice, insurance options, financing and
+          tracking.
+        </p>
+      </div>
     </div>
   );
 }
