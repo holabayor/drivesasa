f8760bd118d6be8d5d01f8bd298b2706046b3c4f
holabayor <liasu.olabayo@gmail.com>
2023-08-29 23:46:49 +0100

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,38 +6,39 @@ import { MoveRight } from 'lucide-react';
 export default function Footer() {
   return (
     <footer className="bg-[#10255A] text-white p-6">
-      <div className="max-width flex-col sm:flex-row flex-between border-b-2 border-gray-100 py-4">
-        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap">
-          {footerLinks.map((footerlink) => (
-            <div
-              key={footerlink.title}
-              className={`flex flex-col ss:my-0 my-4 md:min-w-[150px]`}
-            >
-              <h4 className="font-medium">{footerlink.title}</h4>
-              <ul className="list-none mt-2 md:mt-4">
-                {footerlink.links.map((link, index) => (
-                  <li
-                    key={link.name}
-                    className={`text-xs font-light cursor-pointer ${
-                      index !== footerlink.links.length - 1
-                        ? 'mb-1 md:mb-2'
-                        : 'mb-0'
-                    }`}
-                  >
-                    {link.name}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
+      <div className="max-width flex items-start flex-row flex-wrap justify-between border-b-2 border-gray-100 py-4">
+        {footerLinks.map((footerlink) => (
+          <div
+            key={footerlink.title}
+            className={`flex flex-col my-2 sm:mt-0 md:min-w-[150px]`}
+          >
+            <h4 className="font-medium">{footerlink.title}</h4>
+            <ul className="list-none mt-2">
+              {footerlink.links.map((link, index) => (
+                <li
+                  key={link.name}
+                  className={`text-xs font-light cursor-pointer ${
+                    index !== footerlink.links.length - 1
+                      ? 'mb-1 md:mb-2'
+                      : 'mb-0'
+                  }`}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
 
-        <div className="bg-[#979797] p-4  max-w-[400px]">
-          <h4 className="text-xs font-bold">Subscribe</h4>
+        <div className="bg-[#979797] p-6 max-w-[350px]">
+          <h4 className="text-xs font-bold mb-4">Subscribe</h4>
           <>
-            <div className="relative h-12 w-full">
-              <div className="absolute px-4 top-2/4 rounded-r-md right-0 grid h-full -translate-y-2/4 place-items-center bg-blue-500">
-                <MoveRight size={20} />
+            <div className="group relative h-12 w-full">
+              <div className="absolute px-4 top-2/4 rounded-r-md right-0 grid h-full -translate-y-2/4 place-items-center bg-blue-500 cursor-pointer">
+                <MoveRight
+                  size={20}
+                  className="group-hover:scale-105 -translate-x-2 group-hover:translate-x-2 transition-all duration-300 ease-in-out"
+                />
               </div>
               <input
                 type="email"
@@ -46,13 +47,14 @@ export default function Footer() {
               />
             </div>
           </>
-          <p className="text-xs font-light">
+          <p className="text-xs font-light my-4">
             We are thrilled to share with you very valuable vehicle related
             information for free. Sign up to our newsletter to get yourself in
             the exclusive list.
           </p>
         </div>
       </div>
+      <div className="flex"></div>
     </footer>
   );
 }
