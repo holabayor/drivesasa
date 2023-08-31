1dfc740925c0c5985049d58c023681752482a496
holabayor <liasu.olabayo@gmail.com>
2023-08-31 21:16:37 +0100

    login page

---
+++ app/auth/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from 'next/image';
+
+export default function page() {
+  return (
+    <div className="max-width flex flex-1 max-h-screen">
+      <div>
+        <Image
+          src="/images/woman.png"
+          fill={true}
+          alt="logo"
+          className="object-contain"
+        />
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold">Login here</h1>
+      </div>
+    </div>
+  );
+}
