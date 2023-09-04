fd26c8a229e24bfd10cdbb3ccb2c4bf5dfad0c54
holabayor <liasu.olabayo@gmail.com>
2023-09-04 22:06:21 +0100

    login page

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -5,15 +5,17 @@ import Image from 'next/image';
 export default function page() {
   return (
     <div className="max-width flex flex-1 min-h-screen">
-      <div>
+      <div className="">
         <Image
           src="/images/woman.png"
-          fill={true}
+          height={500}
+          width={500}
+          layout="responsive"
           alt="logo"
-          className="object-cover"
+          className="object-contain"
         />
       </div>
-      <div>
+      <div className="w-[50%]">
         <h1 className="text-4xl font-bold">Login here</h1>
       </div>
     </div>
