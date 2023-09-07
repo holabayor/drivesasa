a7ba2e10d37117697c74af37cc3cf035fe9e9977
holabayor <liasu.olabayo@gmail.com>
2023-09-07 11:30:27 +0100

    Input box

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,13 @@
+import Input from '@/components/Input';
+import React from 'react';
+
+export default function SignUp() {
+  return (
+    <section>
+      SignUp
+      <div>
+        <Input placeholder="Email" id="email" />
+      </div>
+    </section>
+  );
+}
--- components/Input.tsx
+++ components/Input.tsx
@@ -2,21 +2,31 @@
 
 interface InputProps {
   type?: string;
+  id: string;
   placeholder: string;
   disabled?: boolean;
 }
 
 export default function Input({
   type = 'text',
+  id,
   placeholder,
   disabled = false,
 }: InputProps) {
   return (
-    <input
-      type={type}
-      placeholder={placeholder}
-      disabled={disabled}
-      className="border border-gray-300 rounded-md w-full px-3 py-2 mt-1 mb-5 text-sm"
-    />
+    <div className="relative">
+      <input
+        type={type}
+        id={id}
+        disabled={disabled}
+        className="peer border border-gray-300 rounded-md w-full px-3 py-3 my-2 focus:outline-none focus:ring focus:ring-opacity-40 transition-all duration-300 ease-in-out"
+      />
+      <label
+        htmlFor={id}
+        className="ml-4 -mt-11 text-blue-600 peer-placeholder-shown:text-gray-600 peer-placeholder-shown:-mt-8"
+      >
+        {placeholder}
+      </label>
+    </div>
   );
 }
